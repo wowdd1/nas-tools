b108bbc114c117912bcfca42215e513706ab7978
wowdd1 <developergf@gmail.com>
2023-01-22 18:49:31 +0800

    add sync_file.sh for dir sync

--- rclone/unmount.sh
+++ rclone/unmount.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
 
+pid=`pgrep 'rclone'`
+
+echo $pid
+
+if [ "$pid" = "" ]
+then
+  echo "pid is not set!"
+else
+  echo "pid is set !"
+  echo "kill $pid rclone"
+  pgrep 'rclone'|xargs kill -9
+fi
+
+
 DIR="$PWD/../../nas-data"
 
-fusermount -uz $DIR/source 
+echo "unmount $DIR"
+
+fusermount -quz $DIR/source 
+
+#umount $DIR/source
---
+++ sync_file.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+
+export NASTOOL_CONFIG=$PWD/config/config.yaml
+
+echo $NASTOOL_CONFIG
+
+source_dir="$PWD/../nas-data/source"
+target_dir="$PWD/../nas-data/target"
+
+#python3 app/filetransfer.py -m softlink -s $source_dir -d $target_dir
+
+
+
+cd rclone
+
+
+./unmount.sh
+
+nohup ./mount.sh &
+
+
+cd ..
+
+echo "sleep 3 second"
+sleep 3
+
+echo "ls source dir:"
+ls $source_dir
+
+echo $PWD
+
+echo "let nas tools sync"
+
+curl -X 'GET'   'http://localhost:8082/api/v1/sync/run'   -H 'accept: application/json' --header 'Authorization: GlkcvLuoygdevU2P'
+
+
+
+pid=`pgrep 'sftpgo'`
+
+echo $pid
+
+if [ "$pid" = "" ]
+then
+  echo "sftp not running, start sftpgo"
+  cd sftpgo
+  nohup ./sftpgo serve &
+  sleep 1.5
+  pid=`pgrep 'sftpgo'`
+  echo "sftpgo pid: $pid"
+else
+  echo "sftp is running pid: $pid"
+  #echo "kill $pid sftpgo"
+  #pgrep 'sftpgo'|xargs kill -9
+fi
+
